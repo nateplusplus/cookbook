7f0554bee4b4eb078b8021fee9e8d9845cc8833e
Nathan Blair <nateblairfilm@gmail.com>
2015-07-03 13:12:26 -0400

    figuring out fixture issues

--- js/controllers/ingredientsController.js
+++ js/controllers/ingredientsController.js
@@ -3,6 +3,17 @@ Cookbook.IngredientsController = Ember.ArrayController.extend({
         var cupboardItems = this.get('cupboard').getEach('item');
         
         // Find instances of cupboard items within ingredients model
+        var firstRecipe = this.store.find('ingredients', {food: 'Baked Chicken'});
+        console.log(firstRecipe);
+        
+        
+            // Finding matching records
+            //  http://guides.emberjs.com/v1.10.0/models/finding-records/
+        
+            // Change model to actual network request instead of fixture - will solve a lot
+            // http://stackoverflow.com/questions/16753150/unable-to-query-emberjs-model-when-using-ds-fixtureadapter
+        
+        
         // Find matching recipeId for each
         // Get ingredients corresponding to each recipeId
         // Subtract any ingredients already listed in cupboard
